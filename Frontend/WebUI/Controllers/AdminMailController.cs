dc52ab9ff95686ceb12e61ea9edae2f2b6c75454
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-14 00:57:01 +0300

    updated

---
+++ Frontend/WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,47 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using WebUI.Models.Mail;
+
+namespace WebUI.Controllers
+{
+    
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            var token = HttpContext.Session.GetString("AuthToken");
+            if (string.IsNullOrEmpty(token))
+            {
+                return Unauthorized();
+            }
+            MimeMessage message = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin","oguzhanktn0@gmail.com");
+            message.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            message.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            message.Body = bodyBuilder.ToMessageBody();
+            message.Subject = model.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("oguzhanktn0@gmail.com", "srsv rhzp fgvv bxsj");
+            smtpClient.Send(message);
+            smtpClient.Disconnect(true);
+
+            return View();
+        }
+    }
+}
